7ed6b940de3f371c1e0963589570e5a5facdfdf1
Masashi Shiomi <bkmn.31519@gmail.com>
2025-07-18 15:00:30 +0900

    ステータス更新用モーダルを切り出し

---
+++ src/app/myPage/action/ConfirmationUpdateStatusModal.tsx
@@ -0,0 +1,40 @@
+import { Alert } from 'react-native'
+import updateStatus from './backend/updateStatus';
+
+export default function ConfirmationUpdateStatusModal(
+  userId: string,
+  friendId: string,
+  friendUsersId: string,
+  friendDocumentId: string,
+  isBlocked: boolean,
+  setStatus: (status: string) => void,
+  setIsBlocked: (isBlocked: boolean) => void,
+  setIsNotificationEnabled: (isNotificationEnabled: boolean) => void,
+  setIsViewEnabled: (isViewEnabled: boolean) => void
+) {
+  Alert.alert(
+    isBlocked ? '友人のブロック解除' : '友人をブロック',
+    isBlocked ? '友人のブロックを解除しますか？' : '友人をブロックしますか？',
+    [
+      {
+        text: 'キャンセル',
+        style: 'cancel',
+      },
+      {
+        text: isBlocked ? 'ブロック解除' : 'ブロック',
+        style: 'destructive',
+        onPress: async () => updateStatus(
+          userId,
+          friendId,
+          friendUsersId,
+          friendDocumentId,
+          isBlocked,
+          setStatus,
+          setIsBlocked,
+          setIsNotificationEnabled,
+          setIsViewEnabled
+        )
+      },
+    ]
+  );
+}
