c6a9da78a7799607a55cb72cadd8214810548a9f
Masashi Shiomi <bkmn.31519@gmail.com>
2025-07-24 17:34:30 +0900

    感情スコア失敗時の処理を追加

--- src/app/analysis/actions/backend/fetchFeelingScore.ts
+++ src/app/analysis/actions/backend/fetchFeelingScore.ts
@@ -4,22 +4,29 @@ import dayjs from 'dayjs';
 import { FeelingScoreType } from '../../../../../type/feelingScore';
 
 export default function fetchFeelingScore(
+  userId: string,
   setFeelingScoreDates: (feelingScoreDates: FeelingScoreType[]) => void,
   startOfMonth: dayjs.Dayjs,
   endOfMonth: dayjs.Dayjs,
-  userId?: string,
 ) {
-  const ref = collection(db, `users/${userId}/feelingScores`)
-  const q = query(ref, orderBy('diaryDate', 'asc'), where('diaryDate', '>=', startOfMonth.toDate()), where('diaryDate', '<', endOfMonth.toDate()))
-  const unsubscribe = onSnapshot(q, (snapshot) => {
-    const remoteChartData: FeelingScoreType[] = []
-    snapshot.docs.forEach((doc) => {
-      const { diaryDate, feelingScore } = doc.data() as { diaryDate: Timestamp, feelingScore: number};
-      // Timestampオブジェクトを 'M/D' 形式の文字列に変換
-      const formattedDate = dayjs(diaryDate.toDate()).format('M/D');
-      remoteChartData.push({ date: formattedDate, value: feelingScore });
+  if (!userId) return;
+  try {
+    const ref = collection(db, `users/${userId}/feelingScores`)
+    const q = query(ref, orderBy('diaryDate', 'asc'), where('diaryDate', '>=', startOfMonth.toDate()), where('diaryDate', '<', endOfMonth.toDate()))
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const remoteChartData: FeelingScoreType[] = []
+      snapshot.docs.forEach((doc) => {
+        const { diaryDate, feelingScore } = doc.data() as { diaryDate: Timestamp, feelingScore: number};
+        // Timestampオブジェクトを 'M/D' 形式の文字列に変換
+        const formattedDate = dayjs(diaryDate.toDate()).format('M/D');
+        remoteChartData.push({ date: formattedDate, value: feelingScore });
+      })
+      setFeelingScoreDates(remoteChartData)
     })
-    setFeelingScoreDates(remoteChartData)
-  })
-  return unsubscribe;
+    console.log('感情スコアの取得に成功しました');
+    return unsubscribe;
+  } catch (error) {
+    console.error('感情スコアの取得に失敗しました:', error);
+    setFeelingScoreDates([]);
+  }
 }
