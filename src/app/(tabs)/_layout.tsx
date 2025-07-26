5f4460a3b9f5418ad85a8b2ddedfbedd7d1568ae
Masashi Shiomi <bkmn.31519@gmail.com>
2025-07-26 12:05:37 +0900

    タグ表示するファイル名修正

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -73,7 +73,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="mentalHealthCheck"
+        name="mentalHealthCheckList"
         options={{
           title: "メンタルヘルスチェック一覧",
           headerShown: false,
