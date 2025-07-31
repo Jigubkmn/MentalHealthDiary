a6f6282760f4357aa897bdab749c46db348ba274
Masashi Shiomi <bkmn.31519@gmail.com>
2025-07-31 21:48:53 +0900

    引数を修正

--- src/app/myPage/action/ApprovalConfirmationModal.tsx
+++ src/app/myPage/action/ApprovalConfirmationModal.tsx
@@ -1,12 +1,13 @@
 import { Alert } from 'react-native'
-import { FriendInfoType } from '../../../../type/friend'
 import deleteFriend from './backend/deleteFriend'
 import updateApprovalStatus from './backend/updateApprovalStatus';
 
 
 export default function ApprovalConfirmationModal(
   userId: string,
-  friendData: FriendInfoType,
+  friendId: string,
+  friendUsersId: string,
+  userName: string,
   friendDocumentId: string | null,
   onFriendDeleted: (friendId: string) => void,
   setStatus: (status: string) => void,
@@ -14,15 +15,15 @@ export default function ApprovalConfirmationModal(
   // 承認確認モーダルを表示
   Alert.alert(
     '友人承認',
-    `${friendData.userName}さんからの友達申請を承認しますか？`,
+    `${userName}さんからの友達申請を承認しますか？`,
     [
       {
         text: '承認しない',
-        onPress: async () => deleteFriend(userId, friendData, friendDocumentId, onFriendDeleted),
+        onPress: async () => deleteFriend(userId, friendId, friendUsersId, friendDocumentId, onFriendDeleted),
       },
       {
         text: '承認する',
-        onPress: () => updateApprovalStatus(userId, friendData, friendDocumentId, setStatus),
+        onPress: () => updateApprovalStatus(userId, friendId, friendUsersId, friendDocumentId, setStatus),
       },
     ]
   );
