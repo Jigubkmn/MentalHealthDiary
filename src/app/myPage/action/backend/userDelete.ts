dc36e0a9645d0995b1c967822eb01960765ee324
Masashi Shiomi <bkmn.31519@gmail.com>
2025-08-08 16:47:02 +0900

    ユーザーアカウントの削除関数のファイルを作成

---
+++ src/app/myPage/action/backend/userDelete.ts
@@ -0,0 +1,7 @@
+// import { Alert } from 'react-native'
+// import { auth } from '../../../../config';
+// import { router } from 'expo-router';
+
+export default function userDelete() {
+  console.log("アカウント削除しましょう！")
+}
